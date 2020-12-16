6e9e4b085cad1b812beb1e6cadd2d3894ddf3962
Logan Smith <logsmith06@gmail.com>
2020-12-16 17:26:43 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,5 +3,5 @@
 document.addEventListener("DOMContentLoaded", function(){
   console.log("The DOM has loaded");
   let barf = document.getElementById('text');
-  barf.textContent = 'This is really cool!'
+
 });
